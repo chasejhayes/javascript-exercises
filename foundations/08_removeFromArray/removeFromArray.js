fa5625ee32f48679db1ef13ad9cf3f37d9903339
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-24 11:57:05 -0500

    complete exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
-};
+//   let arr = [1,2,3,4,5,6,7]
+  
+//   const removeFromArray = function(arr, ...remove){
+//     arr = remove.filter(newArr => (newArr !== remove))
+//     return arr;
+// };
+
+//   const removeFromArray = function(arr, ...removeArray){
+//         let newArray = exampleArray.filter(individualElementofArray => !removeArray.includes(individualElementofArray))
+//         return newArray; 
+//     }
+
+
+const removeFromArray = function(arr, ...remove){
+        let newArray = arr.filter(arg => !remove.includes(arg))
+        return newArray; 
+    }
+
+
+
+// loop through the argument array comparing to filter
 
 // Do not edit below this line
 module.exports = removeFromArray;
