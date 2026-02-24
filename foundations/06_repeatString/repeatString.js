e43b752a0297d1e9b97fdf471aa97f0b0386d36f
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-24 10:13:03 -0500

    Complete exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,9 +1,16 @@
 
-console.log("Thie")
 
-const repeatString = function() {
+const repeatString = function(string, num) {
+            let newString = '';
+            if (num<0){
+                return "ERROR"
+            } else 
+            for (let i = num; i > 0; i--) {
+            (newString += string)
+            }
+            return newString;
+        }
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
