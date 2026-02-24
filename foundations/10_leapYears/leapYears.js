559cb92e3dd2698326a353a1dcff110564bfc47f
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-24 16:52:06 -0500

    complete exercise 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(isItLeapYear) {
+    if (isItLeapYear%400 == 0){
+        return true
+    } else if (isItLeapYear%100==0 && isItLeapYear%4 == 0){
+        return false
+    } else if (isItLeapYear%4 == 0) {
+        return true
+    } else {
+        return false
+    }
 };
+
+// leap year/4 == true
+// leap year/100 == false
+// leap year/400 == true
+// 1900 is 
 
 // Do not edit below this line
 module.exports = leapYears;
