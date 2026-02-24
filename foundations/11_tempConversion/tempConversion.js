724dc476f8fbdbe1ee3e8d9fae76628cdbe9c2a0
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-24 17:09:48 -0500

    Complete exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempToC) {
+  let c;
+  c = (tempToC - 32) / (9/5);
+  c = c.toFixed(1);
+  return c = Number(c)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempToF) {
+  let f;
+  f = tempToF * 9/5 + 32;
+  f = f.toFixed(1);
+  return f = Number(f);
 };
+
+
+
+// let n = num.toFixed(2);
+// var x = Number("1000")
+// °F = °C * 9/5 + 32
+// °C = (°F - 32) ÷ (9/5)
+
 
 // Do not edit below this line
 module.exports = {
