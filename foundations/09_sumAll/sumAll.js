719d34de913def73ff04eaf4b53b17a8231db90b
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-24 12:44:59 -0500

    complete exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+// const sumAll = function(a, b) {
+//     let sum = 0;
+//     for (i=a; i<b; i++){
+//         sum += a
+//     }
+//     return sum
+// };
 
-};
+// console.log(sumAll(1,10))
+
+   const sumAll = function (a, b) {
+           let sum = 0;
+            let bigger;
+            let smaller;
+    if (a < 0 || b < 0 || typeof(a) === 'string' || typeof(b) ==='string' || typeof(a)==='object' || typeof(b)==='object' ||  Number.isInteger(a) == false || Number.isInteger(b) == false ){
+            return "ERROR"
+        } else
+            if (a>b) {
+                bigger = a, 
+                smaller = b;
+            } else bigger = b, smaller = a
+            for (i = smaller; i < (bigger+ 1); i++) {
+                sum += i
+            }
+            return sum
+        };
+
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
