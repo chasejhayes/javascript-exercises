c41bd07790d5c5319643916d3b225c5828b6edd3
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-24 10:20:49 -0500

    complete exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    arr = arr.reverse();
+    return string = arr.join('')
 };
+
+
+
+// turn string into an array
+// reverse array
+// turn array back into string
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
