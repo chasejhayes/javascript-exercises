8a9d059898679b29622b7d9ca49dbbfb28599e4a
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-03-11 15:08:55 -0500

    Complete js exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (a, b) {
+  return a + b
 
 };
 
-const power = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
+
 };
 
-const factorial = function() {
-	
+const sum = function (number) {
+  return number.reduce((start, current) => start + current, 0)
 };
+
+// const array = [1, 2, 3, 4];
+
+// // 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue,
+// );
+
+
+const multiply = function (numbers) {
+  return numbers.reduce((start, current) => start * current, 1)
+
+};
+
+const power = function (a, b) {
+  return a ** b
+
+};
+
+const factorial = function (number) {
+  let arr = [];
+
+  for (let i = number; i > 0; i--) {
+    arr.push(i)
+  }
+
+  return arr.reduce((start, current) => start*current, 1)
+};
+
+
+
+
+// for (let i=number; i> 0; i--){
+//   return arr.push(i)
+// }
+
+
 
 // Do not edit below this line
 module.exports = {
