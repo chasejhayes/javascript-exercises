66a77c4862fd79064dd887a6a94ae1948e40a8bf
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-03-12 11:06:23 -0500

    Complete js exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,52 @@
-const palindromes = function () {
 
-};
+// const palindromes = function (string) {
+//     let stringOne = string.toLowerCase().split("");
+//     stringOne = stringOne.filter((item)=>{
+//         if (item == "." || item == "," || item == "!"){
+//             return false
+//         } else {
+//             return true
+//         }
+//     })
+//     console.log(stringOne)
+//     console.log(stringOne)
+//     let stringTwo = string.toLowerCase().split("").reverse();
+//     console.log(stringTwo)
+//     stringOne = stringOne.toString();
+//     stringTwo = stringTwo.toString()
+//     console.log(stringTwo)
+//     return stringOne == stringTwo ? true : false
+// };
+
+
+const palindromes = function (string) {
+    let arrOne = string.toLowerCase().split("");
+    let arrOneFiltered = arrOne.filter((item) => {
+        if (item == "." || item == "," || item == "!" || item == " ") {
+            return false
+        } else {
+            return true
+        }
+    })
+
+    let arrTwo = [...arrOneFiltered]
+    arrTwo = arrTwo.reverse();
+
+    arrOneFiltered = arrOneFiltered.join("");
+    arrTwo = arrTwo.join("");
+
+    console.log(arrOneFiltered);
+    console.log(arrTwo);
+
+    return arrOneFiltered == arrTwo ? true : false;
+
+}
+
+
+// convert string into an array and assign to variable
+// verse the array and assign to second variable
+// compare the two
+
 
 // Do not edit below this line
 module.exports = palindromes;
