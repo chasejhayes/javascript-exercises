fafeeb62d58ca9cd99dfc203c38aa861247ca30c
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-03-12 15:10:16 -0500

    Complete exercise 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,34 @@
-const getTheTitles = function() {
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
+
+
+
+
+const getTheTitles = function(books) {
+    return books.map(book=>book.title);
 
 };
+
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
+
+// books.map(book=>book.title);
 
 // Do not edit below this line
 module.exports = getTheTitles;
