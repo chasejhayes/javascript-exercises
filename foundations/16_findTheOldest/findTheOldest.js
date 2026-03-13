c6c413c4c8010073eec8b86f76fc9a1fd2868118
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-03-13 13:26:46 -0500

    Complete exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,57 @@
-const findTheOldest = function() {
+const people = [
+            {
+                name: "Carly",
+                yearOfBirth: 1066,
+            },
+            {
+                name: "Ray",
+                yearOfBirth: 1962,
+                yearOfDeath: 2011,
+            },
+            {
+                name: "Jane",
+                yearOfBirth: 1912,
+                yearOfDeath: 1941,
+            },
+        ]
 
-};
+
+const findTheOldest = function (people) {
+
+    const noDeathDate = (x) => x.yearOfDeath === undefined;
+    const hasDeathDate = (x) => x.yearOfDeath > 0;
+
+    let knownAgeArr = people.filter(hasDeathDate);
+    let unknownAgeArr = people.filter(noDeathDate)
+
+    let newDeathDate = new Date().getFullYear();
+
+    let newlyKnownAgeArr = unknownAgeArr.map(x => {
+        return { ...x, yearOfDeath: newDeathDate }
+    })
+
+
+    let finalFullArray = knownAgeArr.concat(newlyKnownAgeArr);
+
+    let newArr = finalFullArray.map(x => ({
+        ...x, age: (x.yearOfDeath - x.yearOfBirth
+        )
+    }))
+
+    let oldest = newArr.sort((a, b) => a.age < b.age ? 1 : -1);
+    return oldestPerson = oldest[0];
+}
+
+console.log(findTheOldest(people).name)
+
+// add a death date to those people who don't have one
+
+
+
+
+// Given an array of objects representing people with a birth and death year, return the oldest person.
+// - One of the tests checks for people with no death-date.. use JavaScript's Date function to get their age as of today.
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
